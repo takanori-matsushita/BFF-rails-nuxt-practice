c9384916c9aba90f91425a73c52577583d193099
takanori-matsushita <tknr2899@gmail.com>
2024-04-20 18:13:35 +0900

    [add]shadcn

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "./globals.css?url"
+import { LinksFunction } from "@remix-run/node";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
